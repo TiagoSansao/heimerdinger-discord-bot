49456342abe89ef56ddd14a5661ffb6190a13756
Tiago Sansao <tiagossansao@gmail.com>
2020-12-11 11:54:57 -0300

    freeweek command is working

--- index.js
+++ index.js
@@ -4,14 +4,34 @@ import {} from 'dotenv/config.js';
 
 const client = new Discord.Client();
 
+let champions = {};
+
+axios
+  .get(
+    'http://ddragon.leagueoflegends.com/cdn/10.25.1/data/en_US/champion.json'
+  )
+  .then((response) => (champions = response.data.data));
+
+// coloca o request dentro da funçã ode pegar nome, foda-se
+
+function getChampionName(id) {
+  for (let champion in champions) {
+    if (parseInt(champions[champion].key) === id) {
+      return champions[champion].name;
+    }
+  }
+}
+
 async function getFreeWeek() {
   const response = await axios.get(
-    'https://br1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=RGAPI-3f04dbf7-7ccf-4895-8eab-42d4d284236f'
+    `https://br1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=${process.env.RIOT_API_KEY}`
   );
   if (!response) return 'Something went wrong, try again later.';
-  return response.data.freeChampionIds;
+  const FWChampions = response.data.freeChampionIds.map((id) =>
+    getChampionName(id)
+  );
+  return FWChampions;
 }
-console.log(getFreeWeek());
 
 client.on('ready', () => {
   console.log(`Logged as ${client.user.tag}`);
