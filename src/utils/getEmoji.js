efa059f65144190ec2e2c66e89eff84d29f81422
tiago <tiagossansao@gmail.com>
2021-01-06 18:00:19 -0300

    Fixed RegExp error in getEmoji

--- src/utils/getEmoji.js
+++ src/utils/getEmoji.js
@@ -1,7 +1,6 @@
 export default function getEmote(emojiName, cache) {
-  console.log(emojiName);
   emojiName = emojiName.replace(/\s|:/g, "");
-  const regEx = new RegExp(emojiName, "i");
+  const regEx = new RegExp(`^${emojiName}$`, "i");
   const emote = cache.find((emoji) => regEx.test(emoji.name));
   if (!emote) return "**Error send a message to support showing this!**";
   return emote.toString();
